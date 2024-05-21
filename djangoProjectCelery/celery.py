196380ac67ead2f5ad7f99867c6b8ec0fdc0ba11
Tiger <yangtiger2005@163.com>
2024-05-21 11:14:31 +0800

    celery settings

--- djangoProjectCelery/celery.py
+++ djangoProjectCelery/celery.py
@@ -7,7 +7,9 @@ from django.conf import settings
 # djangoProjectCelery 是当前项目名
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProjectCelery.settings')
 django.setup()
-# 实例化一个
+# 实例化一个celery类
 celery_app = Celery('djangoProjectCelery')
+# 指定celery配置文件位置
 celery_app.config_from_object('django.conf:settings')
+# 自动从setting的配置installed_app中的应用目录下加载task.py
 celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
