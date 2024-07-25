5cc54e43824ac854412452d7b5c9d5c1d4107eab
yangtiger2005 <yangtiger2005@163.com>
2024-07-25 14:25:03 +0800

    补充migrant文件

--- djangoProjectCelery/settings.py
+++ djangoProjectCelery/settings.py
@@ -159,7 +159,7 @@ WSGI_APPLICATION = 'djangoProjectCelery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'identifier.sqlite',
     }
 }
 
